1864a4d342bbbb7ca27e963780695a960ec5a597
Roei Meiri <roei.meiri9@hotmail.co.il>
2026-06-21 12:42:25 +0300

    Removing the old links to the Katex library from the HTML

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,7 @@
 import { initUI, renderMathSymbols } from "@app/ui";
 import { renderDiagram } from "@core/render-service";
+import "katex/dist/katex.min.css";
+
 document.addEventListener("DOMContentLoaded", () => {
   initUI();
   renderDiagram();
